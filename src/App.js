8686990691fc721bcdbea290c2fcbb5bfeb085ac
Александр Мартынов <Martinz2003@yandex.ru>
2025-09-13 15:43:20 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+//декларативный
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//императивный
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const year = new Date().getFullYear();
+	return (
+		//декларативный
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					'and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: 'blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('div', null, year),
+			),
+		)
+	);
 }
 
 export default App;
