d0bae7808e172ac8d01a8d4e96bf8c986120b11e
Александр Мартынов <Martinz2003@yandex.ru>
2025-09-21 17:59:27 +0300

    Добавил компонент MyComponent

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-//декларативный
-import { createElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
-//императивный
+
+import styles from './App.module.css';
+import { MyComponent } from './MyComponent';
+
+
 function App() {
-	const year = new Date().getFullYear();
-	return (
-		//декларативный
-		createElement(
-			'div',
-			{ className: 'App' },
-			createElement(
-				'header',
-				{ className: 'App-header' },
-				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-				createElement(
-					'p',
-					null,
-					'Edit ',
-					createElement('code', null, 'src/App.js'),
-					'and save to reload.',
-				),
-				createElement(
-					'a',
-					{
-						className: 'App-link',
-						href: 'https://reactjs.org',
-						target: 'blank',
-						rel: 'noopener noreferrer',
-					},
-					'Learn React',
-				),
-				createElement('div', null, year),
-			),
-		)
-	);
+  
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+		<MyComponent />
+	  </header>
+    
+    </div>
+  );
 }
 
 export default App;
